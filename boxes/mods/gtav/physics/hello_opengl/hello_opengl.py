a04149e4b7663e92809959d78ffe97a9d6f2f75c
kampff <adam.kampff@gmail.com>
2021-11-21 21:38:41 +0000

    Silliness

--- boxes/mods/gtav/physics/hello_opengl/hello_opengl.py
+++ boxes/mods/gtav/physics/hello_opengl/hello_opengl.py
@@ -4,7 +4,7 @@ from OpenGL.GLU import *
 
 def init():
     glClearColor(0.0, 0.0, 0.0, 1.0) 
-    glColor3f(0.2, 0.5, 0.4)
+    glColor3f(1.0, 1.0, 1.0)
     glPointSize(10.0)
     gluOrtho2D(0, 500, 0, 500)
 
@@ -27,13 +27,17 @@ glutCreateWindow("Hello OpenGL")
 # Main Loop
 init()
 x = 100
+y = 500
 dir = 1
+gravity = -0.01
 while(True):
+    #Handle collisons
     if (x > 499):
         dir = -1
     if (x < 1):
         dir = 1
-    x  = (x + dir*0.01)
-    display(x,100)
+    x  = (x + dir*0.05)
+    y = y + gravity
+    display(x,y)
     glutSwapBuffers()
 # FIN
