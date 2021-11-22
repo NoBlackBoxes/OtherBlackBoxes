d5a4fb907776026fb1906761f3ad4e9e45af71e6
kampff <adam.kampff@gmail.com>
2021-11-22 23:33:28 +0000

    More physics denos

--- boxes/mods/gtav/physics/hello_friction/hello_friction.py
+++ boxes/mods/gtav/physics/hello_friction/hello_friction.py
@@ -1,12 +1,14 @@
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
+import time
 
 # Define constants
 screen_width = 500
 screen_height = 500
 screen_x = 100
 screen_y = 100
+gravity = -0.1
 
 # Display function
 def display(x, y):
@@ -35,6 +37,27 @@ gluOrtho2D(0, screen_width, 0, screen_height)   # View matrix
 # Main Loop
 x = screen_width/2
 y = screen_height/2
+vx = 0
+vy = 0
 while(True):
+    # Apply gravity
+    vy += gravity
+
+    # Apply friction
+    vx = vx * 0.995
+    vy = vy * 0.995
+
+    # Translate
+    x = x + vx
+    y = y + vy
+
+    # Bounce?
+    if(y <= 0):
+        vy = -vy
+
+    # Display
     display(x,y)
+
+    # Sleep
+    time.sleep(0.01)
 # FIN
