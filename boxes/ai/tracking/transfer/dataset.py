0c67b987d9c084d4cd6d01c230a7ffdec50c4faf
kampff <adam.kampff@gmail.com>
2023-01-20 11:13:16 +0000

    tweaks to transfer

--- boxes/ai/tracking/transfer/dataset.py
+++ boxes/ai/tracking/transfer/dataset.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 import torch
+from pycocotools.coco import COCO
 
 # Define dataset class (which extends the utils.data.Dataset module)
 class custom(torch.utils.data.Dataset):
@@ -16,11 +17,99 @@ class custom(torch.utils.data.Dataset):
 
     def __getitem__(self, idx):
         image = Image.open(self.image_paths[idx]).convert('RGB')
-        target = self.targets[idx]
+        target = self.targets[idx,:]
         if self.transform:
             image = self.transform(image)
         if self.target_transform:
             label = self.target_transform(label)
         return image, target
+
+# Load dataset
+def prepare(dataset_name, split):
+
+    # Filter train and test datasets
+    image_paths, targets = filter(dataset_name)
+
+    # Split train/test
+    num_samples = len(targets)
+    num_train = int(num_samples * split)
+    num_test = num_samples - num_train
+    indices = np.arange(num_samples)
+    shuffled = np.random.permutation(indices)
+    train_indices = shuffled[:num_train]
+    test_indices = shuffled[num_train:]
+
+    # Bundle
+    train_data = (image_paths[train_indices], targets[train_indices])
+    test_data = (image_paths[test_indices], targets[test_indices])
+
+    return train_data, test_data
+
+# Filter dataset
+def filter(dataset_name):
+
+    # Specify paths
+    coco_folder = '/home/kampff/Dropbox/Voight-Kampff/Technology/Datasets/coco'
+    annotations_path = coco_folder + '/annotations/person_keypoints_' + dataset_name + '.json'
+    images_folder = coco_folder + '/' + dataset_name
+
+    # Initialize the COCO API
+    coco=COCO(annotations_path)
+
+    # Select all people: category and image IDs
+    cat_ids = coco.getCatIds(catNms=['person'])
+    img_ids = coco.getImgIds(catIds=cat_ids )
+
+    # Select annotations of images with only one person with a visible nose
+    image_paths = []
+    targets = []
+    for i in img_ids:
+        ann_ids = coco.getAnnIds(imgIds=i, catIds=cat_ids, iscrowd=None)
+        annotations = coco.loadAnns(ann_ids)
+
+        # Individuals
+        if len(annotations) > 1:
+            continue
+
+        # No crowds
+        if annotations[0]['iscrowd']:
+            continue
+
+        # Extract relevant keypoints
+        keypoints =     annotations[0]['keypoints']
+        nose_x =        keypoints[0]
+        nose_y =        keypoints[1]
+        nose_visible =  keypoints[2]
+        l_eye_x =       keypoints[3]
+        l_eye_y =       keypoints[4]
+        l_eye_visible = keypoints[5]
+        r_eye_x =       keypoints[6]
+        r_eye_y =       keypoints[7]
+        r_eye_visible = keypoints[8]
+
+        # Visible nose and eyes
+        if (nose_visible == 0) or (l_eye_visible == 0) or (r_eye_visible == 0):
+            continue
+        
+        # Big face
+        eye_distance = abs(l_eye_x - r_eye_x) + abs(l_eye_y - r_eye_y)
+        if eye_distance < 40:
+            continue
+
+        # Isolate image path
+        img = coco.loadImgs(annotations[0]['image_id'])[0]
+
+        # Normalize nose centroid
+        width = img['width']
+        height = img['height']
+        x = np.float32(nose_x) / width
+        y = np.float32(nose_y) / height
+        target = np.array([x, y], dtype=np.float32)
+
+        # Store dataset
+        image_paths.append(images_folder + '/' + img['file_name'])
+        targets.append(target)
+
+    return np.array(image_paths), np.array(targets)
 
 #FIN
--- boxes/ai/tracking/transfer/model.py
+++ boxes/ai/tracking/transfer/model.py
@@ -1,5 +1,6 @@
 import torch
 
+
 # Define model (which extends the NN module)
 class custom(torch.nn.Module):
 
@@ -8,47 +9,44 @@ class custom(torch.nn.Module):
         super(custom, self).__init__()
 
         # Load backbone
-        backbone = torch.hub.load('pytorch/vision:v0.10.0', 'resnet50', weights='ResNet50_Weights.DEFAULT')
+        backbone = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', weights='ResNet18_Weights.DEFAULT')
 
         # Freeze the backbone weights
         for param in backbone.parameters():
-            print(param.name)
             param.requires_grad = False
 
         # Remove classifier (i.e. extract feature detection layers)
-        self.features =  torch.nn.Sequential(*list(backbone.children())[:-2])
+        self.features =  torch.nn.Sequential(*list(backbone.children())[:-3])
 
         # Add a new prediction head
-        self.conv = torch.nn.ConvTranspose2d(2048, 640, kernel_size=7, stride=1, bias=False)
-        self.relu1 = torch.nn.ReLU(inplace=True)
+        self.conv = torch.nn.Conv2d(256, 64, kernel_size = (1,1))
         self.flatten = torch.nn.Flatten()
-        self.linear1 = torch.nn.Linear(108160,256)
-        #self.dropout1 = torch.nn.Dropout(0.2)
-        #self.linear2 = torch.nn.Linear(512,256)
-        #self.relu2 = torch.nn.ReLU(inplace=True)
-        #self.dropout2 = torch.nn.Dropout(0.2)
-        #self.linear3 = torch.nn.Linear(64,32)
-        #self.relu3 = torch.nn.ReLU(inplace=True)
-        #self.dropout3 = torch.nn.Dropout(0.2)
-        #self.linear4 = torch.nn.Linear(32,2)
+        self.linear1 = torch.nn.Linear(64*14*14,128)
+        self.relu1 = torch.nn.ReLU(inplace=True)
+        self.dropout1 = torch.nn.Dropout(0.2)
+        self.linear2 = torch.nn.Linear(128,64)
+        self.relu2 = torch.nn.ReLU(inplace=True)
+        self.dropout2 = torch.nn.Dropout(0.2)
+        self.linear3 = torch.nn.Linear(64,32)
+        self.relu3 = torch.nn.ReLU(inplace=True)
+        self.dropout3 = torch.nn.Dropout(0.2)
+        self.linear4 = torch.nn.Linear(32,2)
         self.sigmoid = torch.nn.Sigmoid()
 
     # Forward
     def forward(self, x):
         x = self.features(x)
         x = self.conv(x)
-        x = self.relu1(x)
-        #print(x.size())
         x = self.flatten(x)
+        x = self.relu1(x)
         x = self.linear1(x)
-        #x = self.dropout1(x)
-        #x = self.linear2(x)
-        #x = self.relu2(x)
-        #x = self.dropout2(x)
-        #x = self.linear3(x)
-        #x = self.relu3(x)
-        #x = self.dropout3(x)
-        #x = self.linear4(x)
+        x = self.dropout1(x)
+        x = self.linear2(x)
+        x = self.relu2(x)
+        x = self.dropout2(x)
+        x = self.linear3(x)
+        x = self.relu3(x)
+        x = self.dropout3(x)
+        x = self.linear4(x)
         x = self.sigmoid(x)
         return x
-
