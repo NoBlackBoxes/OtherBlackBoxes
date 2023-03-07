9f76a98d2c0384f4293b1bf972700594cf48aeef
gaspdect <gaspdect@gmail.com>
2023-03-07 21:34:46 +0000

    todays lesson7 march

--- boxes/modeling/book/python/generate_book.py
+++ boxes/modeling/book/python/generate_book.py
@@ -1,11 +1,13 @@
 # Import python libraries
 import os
 import numpy as np
-import matplotlib.pyplot as plt
-import PIL
+
+
 
 # Import blender python libraries
 import bpy
+
+
 
 # Get books collection
 books = bpy.data.collections["Books"]
@@ -13,22 +15,30 @@ books = bpy.data.collections["Books"]
 # Diable active selection
 bpy.context.active_object.select_set(False)
 
-# Get the prototype book and make active
-book = books.objects["Book"]#
+
+ # Get the prototype book and make active
+book = books.objects["Book"]
 for child in book.children:
     bpy.context.view_layer.objects.active = child
     bpy.context.active_object.select_set(True)
 bpy.context.view_layer.objects.active = book
 bpy.context.active_object.select_set(True)
 
-# Make a copy (duplicate) and rename
-bpy.ops.object.duplicate(linked=False)
-new_book = bpy.context.active_object
-new_book.name = "New_Book"
 
-# Get location
-location = new_book.location
-new_book.location.z = location.z + 4
+nbook = 0
+for i in range(5):
+    nbook = nbook + 1
+
+    # Make a copy (duplicate) and rename
+    bpy.ops.object.duplicate(linked=False)
+    new_book = bpy.context.active_object
+    new_book.name = "New_Book" + str(nbook)
+
+    # Get location
+    location = new_book.location
+    new_book.location.z = location.z + 0.4
+    new_book.scale.x = np.random.rand()
+    new_book.scale.y = np.random.rand()
 
 
 
