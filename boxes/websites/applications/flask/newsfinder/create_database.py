36100f246fd4e4e00546751bb489172e4f16d3b6
kampff <adam.kampff@gmail.com>
2023-05-16 17:14:40 +0000

    add newsfinder

---
+++ boxes/websites/applications/flask/newsfinder/create_database.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+# Connect to database (it will create the file on first connection)
+connection = sqlite3.connect('_tmp/database.db')
+
+# Execute the SQL commands in the schema file
+with open('schema.sql') as f:
+    connection.executescript(f.read())
+
+# Commit changes
+connection.commit()
+
+# Close connection
+connection.close()
--- boxes/websites/applications/flask/wsgi/gptvswiki.wsgi
+++ boxes/websites/applications/flask/wsgi/gptvswiki.wsgi
@@ -4,6 +4,6 @@ import sys
 import logging
 logging.basicConfig(stream=sys.stderr)
 sys.path.insert(0,"/var/www/llm/")
-sys.path.insert(0,"/var/www/llm/newsfinder")
-from newsfinder import app as application
+sys.path.insert(0,"/var/www/llm/gptvswiki")
+from gptvswiki import app as application
 application.secret_key = os.getenv("SECRET_KEY")
