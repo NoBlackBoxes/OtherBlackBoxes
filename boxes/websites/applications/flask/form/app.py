264a1beabdf2be9beadc5d84c9a2011de8dbfc47
kampff <adam.kampff@gmail.com>
2023-07-12 12:36:29 +0100

    Update Form app for driving

--- boxes/websites/applications/flask/form/app.py
+++ boxes/websites/applications/flask/form/app.py
@@ -1,5 +1,15 @@
 import os
 from flask import Flask, redirect, render_template, request, url_for
+import serial
+import time
+
+# Configure serial port
+ser = serial.Serial()
+ser.baudrate = 19200
+ser.port = '/dev/ttyUSB0'
+
+# Open serial port
+ser.open()
 
 # Create Flask app
 app = Flask(__name__)
@@ -10,6 +20,12 @@ def index():
     if request.method == "POST":
         direction = request.form["direction"]
         print(direction)
+        if direction == 'Forward':
+            ser.write(b'f')
+            time.sleep(0.05)
+        elif direction == 'Stop':
+            ser.write(b'x')
+            time.sleep(0.05)
 
     return render_template("index.html")
 
