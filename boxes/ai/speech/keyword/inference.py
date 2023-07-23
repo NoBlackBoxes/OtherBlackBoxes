102db4c5b69b3f11756aec11073467027523e198
kampff <adam.kampff@gmail.com>
2023-07-23 17:45:25 +0100

    Working keyword detector

--- boxes/ai/speech/keyword/inference.py
+++ boxes/ai/speech/keyword/inference.py
@@ -1,7 +1,9 @@
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 import torch
 import pyaudio
+from python_speech_features import mfcc
 import sound
 
 # Locals libs
@@ -12,6 +14,33 @@ import importlib
 importlib.reload(model)
 importlib.reload(sound)
 
+# Get user name
+username = os.getlogin()
+
+# Specify paths
+repo_path = '/home/' + username + '/NoBlackBoxes/repos/OtherBlackBoxes'
+box_path = repo_path + '/boxes/ai/speech/keyword'
+model_path = box_path + '/_tmp/sheila.pt'
+
+# Set parameters
+num_mfcc = 16
+len_mfcc = 16
+
+# Load model
+custom_model = model.custom()
+custom_model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+
+# Get cpu or gpu device for training.
+device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
+print(f"Using {device} device")
+
+# Move model to device
+custom_model.to(device)
+
+
+# List sound devices
+#sound.list_devices()
+
 # Initiliaze microphone thread
 microphone = sound.microphone(4, 1600, pyaudio.paInt16, 16000, 10)
 microphone.start()
@@ -20,8 +49,36 @@ microphone.start()
 try:
     while True:
         buffer = microphone.read_latest(16000)
-        avg = np.average(np.abs(buffer))
-        print(avg)
+
+        # Compute MFCCs
+        mfccs = mfcc(buffer, 
+                    samplerate=16000,
+                    winlen=0.100,
+                    winstep=0.064,
+                    numcep=num_mfcc,
+                    nfilt=num_mfcc,
+                    nfft=4096,
+                    preemph=0.0,
+                    ceplifter=0,
+                    appendEnergy=False,
+                    winfunc=np.hanning)
+
+        # Prepare network input
+        input = torch.tensor(np.float32(mfccs.transpose()))
+        input = torch.unsqueeze(torch.unsqueeze(input, 0), 0)
+
+        # Send to GPU
+        input = input.to(device)
+
+        # Inference
+        output = custom_model(input)
+
+        # Extract output
+        output = output.cpu().detach().numpy()
+        output = np.squeeze(output)
+
+        # Report
+        print(output > 0.75)
 
 finally:
     # Shutdown
