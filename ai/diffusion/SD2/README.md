1afe9d95ca71e840d394eef2d1685f3c6ea7d7f8
kampff <adam.kampff@gmail.com>
2023-10-07 12:31:57 +0100

    Restore SD2

---
+++ ai/diffusion/SD2/README.md
@@ -0,0 +1,104 @@
+# AI : diffusion : Stable Diffusion (v2.0)
+
+## Install Stable Diffusion (v2.0)
+
+Clone Repository
+
+```bash
+mkdir tmp
+cd tmp
+git clone https://github.com/Stability-AI/stablediffusion
+```
+
+Create (and enter) Python virtual environment
+
+```bash
+mkdir venv
+cd venv
+python3 -m venv diffusion
+source diffusion/bin/activate
+```
+
+Setup "Diffusion" Environment
+
+```bash
+cd ../stablediffusion
+python3 setup.py develop
+pip install -r requirements.txt
+pip install invisible-watermark
+pip install cutlass
+# pip uninstall torch
+# pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu
+
+# If using diffusers library...
+pip install --upgrade git+https://github.com/huggingface/diffusers.git transformers accelerate scipy
+```
+
+Install Xformers (requires recent nvcc and gcc)
+
+```bash
+cd ..
+git clone https://github.com/facebookresearch/xformers.git
+cd xformers
+git submodule update --init --recursive
+pip install -r requirements.txt
+pip install -e .
+cd ../stablediffusion
+```
+
+Download weights
+
+```bash
+mkdir weights
+cd weights
+wget https://huggingface.co/stabilityai/stable-diffusion-2-base/resolve/main/512-base-ema.ckpt
+wget https://huggingface.co/stabilityai/stable-diffusion-2/resolve/main/768-v-ema.ckpt
+# Note: 4.9 Gigabytes
+cd ..
+```
+
+Test model
+
+```bash
+# Base model
+python scripts/txt2img.py --prompt "a blueprint style drawing of a school designed by buckminster fuller" --ckpt weights/512-base-ema.ckpt --config configs/stable-diffusion/v2-inference.yaml --H 256 --W 256
+
+# V model
+python scripts/txt2img.py --prompt "professional photograph of a school designed by buckminster fuller" --ckpt weights/768-v-ema.ckpt --config configs/stable-diffusion/v2-inference-v.yaml --H 768 --W 768
+```
+
+## ONNX
+
+Convert stable diffusion to ONNX format
+
+```bash
+pip install onnx
+pip install onnxruntime
+pip install onnxruntime-openvino # Intel
+cd tmp
+git clone https://github.com/huggingface/diffusers
+mkdir onnx
+python diffusers/scripts/convert_stable_diffusion_checkpoint_to_onnx.py \
+  --model_path="stabilityai/stable-diffusion-2" \
+  --output_path="onnx/stablediffusion_onnx"
+```
+
+
+Build OpenVINO
+
+```bash
+git clone https://github.com/openvinotoolkit/openvino.git
+cd openvino
+git submodule update --init --recursive
+mkdir build && cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make --jobs=$(nproc --all)
+```
+
+# NVIDIA GPUs
+
+# AMD GPUs
+```bash
+pip uninstall torch
+pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/rocm5.2
+```
---
+++ ai/diffusion/SD2/python/inspect.py
@@ -0,0 +1,142 @@
+from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler
+import torch
+import matplotlib.pyplot as plt
+
+# Check for GPU (also works for AMD GPUs using ROCm)
+gpu_available = torch.cuda.is_available()
+print("Is the GPU available? {0}".format(gpu_available))
+
+# Set model ID
+model_id = "stabilityai/stable-diffusion-2-1"
+
+# Use the Euler scheduler here instead
+scheduler = EulerDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
+
+# Load models (Large!)
+pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler, revision="fp16", torch_dtype=torch.float16)
+
+# Send to device
+if(gpu_available):
+    pipe = pipe.to("cuda")
+else:
+    pipe = pipe.to("cpu")
+
+# Something
+pipe.enable_attention_slicing()
+
+# Set prompts
+positive_prompt = 'old timey black and white photograph of a robot baby swiping on at a smartphone'
+negative_prompt = 'out of frame, lowres, text, error, cropped, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, out of frame, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck, username, watermark, signature,'
+#negative_prompt = ''
+
+
+# Set height and width based on UNet size and VAE scale factor
+# - pipe.unet.config.sample_size = 64 / 96
+# - pipe.vae_scale_factor = 8
+#height = 512
+#width = 512
+height = 768
+width = 768
+
+# Check inputs. Raise error if not correct
+pipe.check_inputs(positive_prompt, height, width, 1)
+
+# Define call parameters
+batch_size = 1
+device = pipe._execution_device
+guidance_scale = 9.5 # greater than 1 means do classifier free guidance
+do_classifier_free_guidance = True
+
+# Encode input prompt
+num_images_per_prompt = 1
+text_embeddings = pipe._encode_prompt(positive_prompt, device, num_images_per_prompt, do_classifier_free_guidance, negative_prompt)
+
+# Prepare timesteps
+num_inference_steps = 50
+pipe.scheduler.set_timesteps(num_inference_steps, device=device)
+timesteps = pipe.scheduler.timesteps
+
+# Prepare latent variables (random initialize)
+generator = None
+latents = None
+num_channels_latents = pipe.unet.in_channels
+latents = pipe.prepare_latents(
+    batch_size * num_images_per_prompt,
+    num_channels_latents,
+    height,
+    width,
+    text_embeddings.dtype,
+    device,
+    generator,
+    latents,
+)
+
+# Visualize initial latents
+if(not gpu_available):
+    plt.subplot(2,2,1)
+    plt.imshow(latents[0, 0, :, :])
+    plt.subplot(2,2,2)
+    plt.imshow(latents[0, 1, :, :])
+    plt.subplot(2,2,3)
+    plt.imshow(latents[0, 2, :, :])
+    plt.subplot(2,2,4)
+    plt.imshow(latents[0, 3, :, :])
+    plt.show()
+
+## Decode random latents
+#with torch.no_grad():
+#    image = pipe.decode_latents(latents)
+#
+## Visualize initial decoded latents
+#plt.imshow(image[0,:,:,:])
+#plt.show()
+
+# Prepare extra step kwargs
+eta = 0.0
+extra_step_kwargs = pipe.prepare_extra_step_kwargs(generator, eta)
+
+# Denoising loop
+num_warmup_steps = len(timesteps) - num_inference_steps * pipe.scheduler.order
+with torch.no_grad():
+    for i, t in enumerate(timesteps):
+        # expand the latents if we are doing classifier free guidance
+        latent_model_input = torch.cat([latents] * 2) if do_classifier_free_guidance else latents
+        latent_model_input = pipe.scheduler.scale_model_input(latent_model_input, t)
+
+        # predict the noise residual
+        # Inputs:
+        #  - latents (64x64 or 96x96), t (timestep), hidden_states ()
+        noise_pred = pipe.unet(latent_model_input, t, encoder_hidden_states=text_embeddings).sample
+
+        # perform guidance
+        if do_classifier_free_guidance:
+#        if False:
+            noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
+            # UNet precicted noise in text and image embedding....the predicted noise is stepped by the difference between
+            #   them scaled by the "guidance_scale" factor
+            noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
+
+            # compute the previous noisy sample x_t -> x_t-1
+            # Steps the latents...impements diffusion algorithm (subtract oredicited noise...basically...depends a bit on size of timestep...I guess to make it stable...)
+            # - moves latemts through atent space towards the manifold with valid images...bu...guided by the text prompt...
+            latents = pipe.scheduler.step(noise_pred, t, latents, **extra_step_kwargs).prev_sample
+        else:
+            noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
+            noise_pred = noise_pred_uncond
+            # Just denoise initial random latents
+            latents = pipe.scheduler.step(noise_pred, t, latents, **extra_step_kwargs).prev_sample
+
+        # Decode current latents
+        image = pipe.decode_latents(latents)
+
+        # Save current image
+        image = pipe.numpy_to_pil(image)
+        image[0].save("/home/kampff/Downloads/steps/" + str(i).zfill(2) + '.png')
+
+        # Report progress
+        print("Iteration {0}...".format(i))
+
+# Report Done
+print("Done")
+
+#FIN
