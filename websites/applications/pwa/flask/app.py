0007229e0ae7b68607be73d60cb0424294a401a1
kampff <adam.kampff@gmail.com>
2024-03-20 16:03:31 +0000

    Uploads in PWA

--- websites/applications/pwa/flask/app.py
+++ websites/applications/pwa/flask/app.py
@@ -1,6 +1,18 @@
+import os
 from flask import Flask, render_template, send_file
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+# Define constants
+UPLOAD_FOLDER = '/home/kampff/NoBlackBoxes/OtherBlackBoxes/websites/applications/pwa/flask/_tmp'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 # Serve manifest (for PWA)
 @app.route('/manifest.json')
@@ -13,9 +25,25 @@ def serve_sw():
     return send_file('sw.js', mimetype='application/javascript')
 
 # Serve home
-@app.route('/')
-def index():
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return render_template('index.html')
     return render_template('index.html')
+
 if __name__ == '__main__':
     app.run()
 
